0240591e3e5621fa3a12da5557b64195ab48562e
Jason Chen <jasonc020288@gmail.com>
2017-05-23 05:00:55 +0800

    git author rewrite

---
+++ git-author-rewrite.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+git filter-branch --env-filter '
+OLD_EMAIL="jason.c.shun@gmail.com"
+CORRECT_NAME="Jason Chen"
+CORRECT_EMAIL="jasonc020288@gmail.com"
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
